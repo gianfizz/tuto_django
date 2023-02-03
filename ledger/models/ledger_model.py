8d9e0d82760f64ab91a5847fe3d1127bf13bba57
Gian <gianmmarelli@gmail.com>
2023-02-03 10:02:45 -0300

    tarea barba

---
+++ ledger/models/ledger_model.py
@@ -0,0 +1,19 @@
+import uuid
+from django.db import models
+from ledger.models.account_model import AccountModel
+
+
+class LedgerType(models.Choices):
+    IN = "IN"
+    OUT = "OUT"
+
+
+class LedgerModel(models.Model):
+    uuid = models.UUIDField(default=uuid.uuid4, primary_key=True)
+    client_id = models.ForeignKey(AccountModel, on_delete=models.DO_NOTHING)
+    ledger_type = models.CharField(max_length=12, choices=LedgerType.choices)
+    amount = models.FloatField(default=0.0)
+    description = models.TextField(default="No description", blank=True)
+
+    def __str__(self):
+        return f"{self.client_id}, type: {self.ledger_type}, amount: {self.amount}"
